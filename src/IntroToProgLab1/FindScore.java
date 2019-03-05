afe32421fdbf8838ab599088ff7f485de2a25c1c
Yaroslav Obruch <Stamin4ik4@gmail.com>
2019-03-05 13:32:59 +0200

    create directory with csvFiles and create function to read file

--- src/IntroToProgLab1/FindScore.java
+++ src/IntroToProgLab1/FindScore.java
@@ -1,4 +1,27 @@
 package IntroToProgLab1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class FindScore {
+    static final int amountOfTeams=20;
+    static File file = new File("/home/yarik/IdeaProjects/IntroToProg/src/IntroToProgLab1/csvFiles/premier_league.csv");
+
+    static public String[] readFile(File file) {
+        String [] arrayOfStats=new String[amountOfTeams]; //таблица результатов матчей в массиве
+        try (Scanner read = new Scanner(new FileReader(file))) {
+            for (int itter = 0; itter < amountOfTeams; itter++) {
+                arrayOfStats[itter]=read.nextLine();
+            }
+            System.out.println(Arrays.toString(arrayOfStats));
+        } catch (FileNotFoundException e1) {
+            e1.printStackTrace();
+        }
+        return arrayOfStats;
+    }
+
 }
+
